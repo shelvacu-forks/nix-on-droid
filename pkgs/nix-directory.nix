5aa7ea1c0c876aa4ed79a590fb580eaf700db62a
Shelvacu <git@shelvacu.com>
2025-12-23 20:00:14 -0800

    stuff

--- pkgs/nix-directory.nix
+++ pkgs/nix-directory.nix
@@ -1,30 +1,17 @@
 # Copyright (c) 2019-2025, see AUTHORS. Licensed under MIT License, see LICENSE.
 
-{ config
-, lib
-, runCommand
+{ runCommand
 , closureInfo
 , prootTermux
-, proot
 , pkgsStatic
-, system
 , nix
 , cacert
 , bashNonInteractive
+, config
 }:
 
 let
-  buildRootDirectory = "root-directory";
-
   static-nix = pkgsStatic.nix; #.overrideAttrs { version = "omitted"; };
-
-  prootCommand = lib.concatStringsSep " " [
-    "${proot}/bin/proot"
-    "-b ${static-nix}:/static-nix"
-    "-b /proc:/proc" # needed because tries to access /proc/self/exe
-    "-r ${buildRootDirectory}"
-    "-w /"
-  ];
 
   info = closureInfo {
     rootPaths = [
@@ -33,14 +20,9 @@ let
       nix
       cacert
       bashNonInteractive
+      config.build.activationPackage
     ];
   };
-  # prootTermuxClosure = closureInfo {
-  #   rootPaths = [
-  #     prootTermux
-  #     static-nix
-  #   ];
-  # };
 in
 runCommand "nix-directory" { } ''
   # create nix state directory to satisfy nix heuristics to recognize the manual created /nix directory as a valid nix store
