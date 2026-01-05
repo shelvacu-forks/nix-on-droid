fa73f3861e3e5316456e87483793ee67be038bff
Shelvacu <git@shelvacu.com>
2026-01-04 16:51:23 -0800

    stuff

--- pkgs/proot-termux/default.nix
+++ pkgs/proot-termux/default.nix
@@ -41,7 +41,7 @@ stdenv.mkDerivation {
     #define ASHMEM_SET_SIZE _IOW(__ASHMEMIOC, 3, size_t)
     #define ASHMEM_GET_SIZE _IO(__ASHMEMIOC, 4)
     EOF
-    substituteInPlace src/arch.h --replace \
+    substituteInPlace src/arch.h --replace-fail \
       '#define HAS_LOADER_32BIT true' \
       ""
     ! (grep -F '#define HAS_LOADER_32BIT' src/arch.h)
